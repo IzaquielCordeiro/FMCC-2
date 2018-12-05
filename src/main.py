fa629bcfff1457f0eb11b115d74d72997515620f
mateustranquilino <mateus.tranquilino@ccc.ufcg.edu.br>
2018-12-05 14:59:27 -0300

    Scripts implementados

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import cv2 as cv
 
-img1 = cv.imread('../input/imagem3.jpg')
-img2 = cv.imread('../input/imagem4.jpg')
-
+img1 = cv.imread('../input/imagem2.jpg')
+img2 = cv.imread('../input/imagem1.jpg')
 
 def none(x):
     pass
 
 def fadeApply(img1, img2, alpha):
+    
     newImage = img1 * alpha + img2 * (1 - alpha)
 
     path = "../output/img_fade_" + str(alpha) + ".jpg"
@@ -15,16 +15,15 @@ def fadeApply(img1, img2, alpha):
 
     return cv.imread(path)
 
-
-if __name__ == '__main__':
+def main():
+	    
     alpha = 0
-
     cv.namedWindow('Set Alpha (Press ESC to leave)')
     cv.createTrackbar('Alpha', 'Set Alpha (Press ESC to leave)', 0, 100, none)
 
     img = img1
 
-    while(1):
+    while( True ):
 
         cv.imshow('Set Alpha (Press ESC to leave)', img)
         k = cv.waitKey(1) & 0xFF
@@ -34,3 +33,5 @@ if __name__ == '__main__':
         alpha = cv.getTrackbarPos('Alpha', 'Set Alpha (Press ESC to leave)')/100.0
         img = fadeApply(img1, img2, alpha)
 
+if __name__ == "__main__":
+	main()
